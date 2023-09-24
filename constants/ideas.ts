7399e77dd95b6b5c656014b32c3aa6edfb451963
sunpm <isunpm@qq.com>
2023-09-24 17:00:20 +0800

    放入路由

--- constants/ideas.ts
+++ constants/ideas.ts
@@ -9,6 +9,10 @@ export const info: Idea[] = [
     name: 'Bin2Dec',
     date: '2023/09/24',
   },
+  {
+    name: 'Border-radius',
+    date: '2023/09/24',
+  },
 ]
 
 export const ideas = info.map((item, idx) => {
